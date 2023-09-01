bbea15037a647ed89eb1b45548a200976db3d16c
Emin Kumru <eminkumru135@outlook.com>
2023-09-02 01:24:24 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Card from "./components/RestaurantCard";
+
+export default function Home() {
+  return (
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        <Navbar />
+
+        <main>
+          <Header />
+          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+            <Card />
+          </div>
+        </main>
+      </main>
+    </main>
+  );
+}
