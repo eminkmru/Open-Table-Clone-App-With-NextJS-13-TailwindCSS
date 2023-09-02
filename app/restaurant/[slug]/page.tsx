88f45b81ce06c0074f56f75c569187994caf2a1b
Emin Kumru <eminkumru135@outlook.com>
2023-09-02 14:13:15 +0300

    connecting postrgresql and defining sql schema

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
 import Description from "./components/Description";
 import Header from "./components/Header";
 import Images from "./components/Images";
@@ -11,26 +10,19 @@ import Title from "./components/Title";
 
 const RestaurantDetails = () => {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        {/* DESCRIPTION PORTION */}
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
   );
 };
 
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,24 +1,19 @@
-import Link from "next/link";
 import React from "react";
-import Navbar from "../components/Navbar";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSidebar from "./components/SearchSidebar";
 
 const Seach = () => {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
-        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          <SearchSidebar />
-          <div className="w-5/6">
-            <RestaurantCard />
-          </div>
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSidebar />
+        <div className="w-5/6">
+          <RestaurantCard />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 };
 
