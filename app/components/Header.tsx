5612f82d3ab6b745e79354987815be15e957a6a8
Emin Kumru <eminkumru135@outlook.com>
2023-09-02 12:08:19 +0300

    some More Refactoring

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,7 @@
-"use client";
-
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import Searchbar from "./Searchbar";
 
 const Header = () => {
-  const [location, setLocation] = useState<string>();
-  const router = useRouter();
   return (
     <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
       <div className="text-center mt-10">
@@ -13,24 +9,8 @@ const Header = () => {
           Find your table for any occasion
         </h1>
         {/* SEARCH BAR */}
-        <div className="text-left text-lg py-3 m-auto flex justify-center">
-          <input
-            className="rounded  mr-3 p-2 w-[450px]"
-            type="text"
-            placeholder="State, city or town"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <button
-            className="rounded bg-red-600 px-9 py-2 text-white"
-            onClick={() => {
-              if (location === "banana") return;
-              router.push(`/search`);
-            }}
-          >
-            Let's go
-          </button>
-        </div>
+        <Searchbar />
+
         {/* SEARCH BAR */}
       </div>
     </div>
